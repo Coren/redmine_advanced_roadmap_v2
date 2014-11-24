5ff3a6f3e07824163bc576889da641e3522af118
coren <mloiseleur@ekylibre.com>
2014-11-24 23:27:25 +0100

    Fix milestone scoped find. Closes #2. Thanks goes to sealemar.

--- lib/advanced_roadmap/query_patch.rb
+++ lib/advanced_roadmap/query_patch.rb
@@ -4,17 +4,15 @@ module AdvancedRoadmap
   module QueryPatch
     def self.included(base)
       base.class_eval do
-  
         # Returns the milestones
         # Valid options are :conditions
         def milestones(options = {})
-          Milestone.find(:all,
-                         :include => :project,
-                         :conditions => Query.merge_conditions(project_statement, options[:conditions]))
+          Milestone.scoped(:conditions => options[:conditions]).find :all,
+            :include => :project,
+            :conditions => project_statement
         rescue ::ActiveRecord::StatementInvalid => e
           raise StatementInvalid.new(e.message)
         end
-  
       end
     end
   end
