6f4df7fb76725656d8bca73368c9c90752806d5b
coren <michel.loiseleur@free.fr>
2015-07-13 21:29:01 +0200

    Try to fix #53

--- lib/advanced_roadmap/query_patch.rb
+++ lib/advanced_roadmap/query_patch.rb
@@ -7,7 +7,13 @@ module AdvancedRoadmap
         # Returns the milestones
         # Valid options are :conditions
         def milestones(options = {})
-          Milestone.where(options[:conditions]).where(project_statement).joins(:project).all
+          # See https://github.com/Coren/redmine_advanced_roadmap_v2/issues/53
+          # and https://github.com/nanego/redmine_multiprojects_issue/blob/master/lib/redmine_multiprojects_issue/issue_patch.rb
+          if Redmine::Plugin.registered_plugins[:redmine_multiprojects_issue]
+            Milestone.where(options[:conditions]).where(project_statement).joins(project: issues).all
+          else
+            Milestone.where(options[:conditions]).where(project_statement).joins(:project).all
+          end
         rescue ::ActiveRecord::StatementInvalid => e
           raise ::ActiveRecord::StatementInvalid.new(e.message)
         end
